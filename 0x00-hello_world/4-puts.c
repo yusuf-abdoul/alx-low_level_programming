62c4df25a026e1f537d6abda05f95494441c3e2e
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2022-03-14 18:50:05 -0700

    4-puts added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
+ *
  * This is the main functio, where everything holds
  * Return: Always 0(Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
