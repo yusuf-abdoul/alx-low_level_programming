32637c9403d7e09188c398827987494d81bd50b1
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2022-03-14 19:54:20 -0700

    5-printf.c added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this is a great function where things occur
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
