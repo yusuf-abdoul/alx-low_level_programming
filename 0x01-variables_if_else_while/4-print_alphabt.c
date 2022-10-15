918bff95c022dc95a227fa800856966ef1561eae
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-10-14 20:13:54 -0700

    4-print_alphabt.c added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case excluding q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		if (az != 'e' && az != 'q')
+		       putchar(az);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
