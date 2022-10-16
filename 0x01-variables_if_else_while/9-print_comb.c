081cd184b4e577a2a61519defd2b528f81df13d1
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-10-16 06:20:26 -0700

    9-print_comb.c added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinaions of single-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 38; n < 48; n++)
+	{
+		putchar(n);
+		if (n != 47)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n')
+	return (0);
+}
