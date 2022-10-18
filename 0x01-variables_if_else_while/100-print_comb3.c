df1dd25c2daf764817a57f138b2513db64d8f9cf
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-10-17 18:09:43 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ int main(void)
 
 	for (digit_1 = 0; digit_1 < 9; digit_1++)
 	{
-		for (digit _2 = digit_1; digit_2 < 10; digit++)
+		for (digit_2 = digit_1; digit_2 < 10; digit_2++)
 		{
 			putchar((digit_1 % 10) + '0');
-			putchar((digit_2 % 10) + '0')
+			putchar((digit_2 % 10) + '0');
 
 			if (digit_1 == 8 && digit_2 == 9)
 
