21de1f0f4212ec35484b4e9a975dfc0feae4d5ae
yusuf-abdoul <40715160+yusuf-abdoul@users.noreply.github.com>
2022-10-22 05:06:43 +0100

    Update 10-triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,21 @@
  */
 void print_triangle(int size)
 {
-	int row = 0;
-	int column = 0;
-	int row_column_limit = 0;
+	int a, t
+        if (size > 0)
+        {
+            for (a = 1; a <= size; a++)
+            {
+                for (t = size - a; t < a; t--)
+                      _putchar(' ');
+                for (t = 0; t < a; t++)
+                      _putchar('#');
 
-	if (size <= 0)
-		_putchar('\n');
-	else
-	{
-		for (row = 0; row < size; row++)
-		{
-			for (column = 0; column < row_column_limit; column++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-			row_column_limit++;
-		}
+                if (a == size)
+                    continue;
+                _putchar('\n');
 
-	}
+	    }
+	 }
+         _putchar('\n');
 }
