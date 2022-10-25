0569c7f2313937b3fd39a79997453802c2d649bb
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-10-24 20:07:51 -0700

    "updated debugging files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int x, int y, int z);
+int convert_day(int monthes and int days);
+void print_remaining_days(int month, int day, int year);
 
 #endif /*MAIN_H*/
