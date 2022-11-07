1a2a0117277a9b031f13eca791f725d834e745eb
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-11-07 02:53:26 -0800

    6-cap_string.c added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,28 @@
-#include "main.h"
+#include <stddef.h>
 
 /**
- * cap_string - capitilizes words
- * @str: string
- * Return: pointer to string
+ *cap_string - capitalizes all words of a string
+ *@s: the string
+ *
+ *Return: A pointer to the string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i = 0;
+	char *seps = " \t\n\r,;.!?\"(){}";
+	char *ret = s;
+	char *sep = NULL;
 
-	while (str[i])
+	while (*s != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		i++;
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' || str[i - 1] == ',' ||
-		    str[i - 1] == ';' || str[i - 1] == '.' ||
-		    str[i - 1] == '!' || str[i - 1] == '?' ||
-		    str[i - 1] == '"' || str[i - 1] == '(' ||
-		    str[i - 1] == ')' || str[i - 1] == '{' ||
-		    str[i - 1] == '}' || i == 0)
-			str[i] - 32;
-		i++;
+		if ((sep == NULL || *sep != '\0') && 'a' <= *s && *s <= 'z')
+			*
+			s = 'A' + *s - 'a';
+
+		sep = seps;
+		while (*sep != '\0' && *sep != *s)
+			sep++;
+		s++;
 	}
 
-	return (str);
+	return (ret);
 }
